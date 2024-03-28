5df631df0798e43af7385e9713b5fad6c12150d7
Kalyan Basso <kalyan.basso@acqio.com.br>
2024-03-28 14:18:16 -0300

    feat: add dependencies and update App.tsx file

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { usePushNotifications } from "./usePushNotification";
 
 export default function App() {
+  const { expoPushToken, notification } = usePushNotifications();
+
+  const data = JSON.stringify(notification?.request.content, null, 2);
+
+  console.log("expoPushToken", expoPushToken);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Expo Push Token: {expoPushToken?.data}</Text>
+      <Text>{data}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
