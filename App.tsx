54d930bb64be34cd0d74b1141d9150b3d5c3da44
Kalyan Basso <kalyan.basso@acqio.com.br>
2024-04-03 09:45:00 -0300

    Adiciona rastreamento de eventos de análise ao aplicativo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { usePushNotifications } from "./usePushNotification";
+import analytics from "@react-native-firebase/analytics";
+
+analytics().setAnalyticsCollectionEnabled(true);
+
+analytics()
+  .logEvent("app_opened")
+  .then(() => {
+    console.log("event logged successfully");
+  });
 
 export default function App() {
   const { expoPushToken, notification } = usePushNotifications();
 
   const data = JSON.stringify(notification?.request.content, null, 2);
+
+  const handlePressButton = async () => {
+    await analytics()
+      .logSelectContent({
+        content_type: "button",
+        item_id: "button-1",
+      })
+      .then(() => {
+        console.log("event logged successfully");
+      });
+    console.log("Button pressed");
+  };
 
   console.log("expoPushToken", expoPushToken);
 
@@ -13,6 +34,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Expo Push Token: {expoPushToken?.data}</Text>
       <Text>{data}</Text>
+      <Button title="Press me" onPress={handlePressButton} />
       <StatusBar style="auto" />
     </View>
   );
