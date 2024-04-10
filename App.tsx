a2c4d531a5c71b2efe8aee946d52623cb552fa74
Kalyan Basso <kalyan.basso@acqio.com.br>
2024-04-10 15:06:47 -0300

    Atualiza rastreamento de eventos de análise e coleta de erros no aplicativo

--- App.tsx
+++ App.tsx
@@ -1,88 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
-import { usePushNotifications } from "./usePushNotification";
-import analytics from "@react-native-firebase/analytics";
+import { StyleSheet, View } from "react-native";
 
-import crashlytics from "@react-native-firebase/crashlytics";
-import { useEffect } from "react";
-
-analytics().setAnalyticsCollectionEnabled(true);
-
-analytics()
-  .logEvent("app_opened")
-  .then(() => {
-    console.log("event logged successfully");
-  });
-
-analytics()
-  .logScreenView({
-    screen_name: "home",
-    screen_class: "home",
-  })
-  .then(() => {
-    console.log("event logged successfully");
-  });
-crashlytics().setCrashlyticsCollectionEnabled(true);
-console.log(crashlytics().isCrashlyticsCollectionEnabled);
+import { CrashlyticsPoc } from "./src/pocs/CrashlyticsPoc";
+import { AnalyticsPoc } from "./src/pocs/AnalyticsPoc";
+import { PushNotificationPoc } from "./src/pocs/PushNotificationPoc";
 
 export default function App() {
-  const { expoPushToken, notification } = usePushNotifications();
-
-  const data = JSON.stringify(notification?.request.content, null, 2);
-
-  async function onSignIn(user: any) {
-    crashlytics().log("User signed in.");
-    await Promise.all([
-      crashlytics().setUserId(user.uid),
-      crashlytics().setAttribute("credits", String(user.credits)),
-      crashlytics()
-        .setAttributes({
-          role: "admin",
-          followers: "13",
-          email: user.email,
-          username: user.username,
-        })
-        .then(() => {
-          console.log("attributes set successfully");
-        }),
-    ]);
-  }
-
-  const handlePressButton = async () => {
-    await analytics()
-      .logSelectContent({
-        content_type: "button",
-        item_id: "button-1",
-      })
-      .then(() => {
-        console.log("event logged successfully");
-      });
-    console.log("Button pressed");
-  };
-
-  console.log("expoPushToken", expoPushToken);
-
-  useEffect(() => {
-    crashlytics().log("App mounted.");
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Expo Push Token: {expoPushToken?.data}</Text>
-      <Text>{data}</Text>
-      <Button title="Press me" onPress={handlePressButton} />
-      <Button
-        title="Sign In"
-        onPress={() =>
-          onSignIn({
-            uid: "Aa0Bb1Cc2Dd3Ee4Ff5Gg6Hh7Ii8Jj9",
-            username: "Joaquin Phoenix",
-            email: "phoenix@example.com",
-            credits: 42,
-          })
-        }
-      />
-      <Button title="Test Crash" onPress={() => crashlytics().crash()} />
+      <PushNotificationPoc />
+
+      <AnalyticsPoc />
+
+      <CrashlyticsPoc />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -92,8 +23,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    padding: 30,
     gap: 10,
   },
 });
